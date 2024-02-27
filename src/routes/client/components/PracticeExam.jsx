52c70c478d3d5a5e4a27c9c4f563ad7753ebb9e6
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-02-27 02:54:14 +0100

    Udated the practice exam component

--- src/routes/client/components/PracticeExam.jsx
+++ src/routes/client/components/PracticeExam.jsx
@@ -2,12 +2,12 @@ import ReadyTtYn from "../../../assets/images/borrowed/practice-exam.d41f1e39.pn
 export default function PracticeExam() {
   return (
     <>
-      <div className="flex gap-5 bg-white rounder-lg mt-5 shadow-md p-5 rounded-lg items-center pl-10 pr-10">
+      <div className="flex gap-5 pb-0 bg-white rounder-lg mt-5 shadow-md p-5 rounded-lg items-center pl-10 pr-10">
         <div className="w-1/2">
             <div className="mt-2 uppercase text-sm text-orange-400">Practice Exam</div>
             <div className="mt-5 font-bold text-2xl">Ready to test your knowledge?</div>
             <div className="mt-3 text-gray-500">Take practice exams to prepare for upcoming exams. Practice makes perfect grades!</div>
-            <div className="mt-5 mb-3 pt-3 pb-3 pl-6 pr-6 rounded-lg bg-blue-400 text-white hover:bg-blue-500 duration-300 cursor-pointer w-fit">
+            <div className="mt-5 mb-5 pt-3 pb-3 pl-6 pr-6 rounded-lg bg-blue-400 text-white hover:bg-blue-500 duration-300 cursor-pointer w-fit">
                 <button className="uppercase">Start Practice</button>
             </div>
         </div>
