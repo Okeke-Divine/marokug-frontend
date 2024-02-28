45093be9e7fcae634611b50a7c69a6c8e6ea43d1
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-02-28 16:23:14 +0100

    Updated

--- src/routes/client/components/ClientNavbarComponent.jsx
+++ src/routes/client/components/ClientNavbarComponent.jsx
@@ -8,7 +8,7 @@ export default function ClientNavbarComponent() {
           <input
             type="search"
             placeholder="What would you like to learn?"
-            className="bg-transparent w-[8rem] md:w-40 lg:w-80 outline-none"
+            className="bg-transparent w-[8rem] md:w-40 lg:w-80 outline-none text-white md:text-black"
           />
           <button>
             <i className="fi fi-rr-search text-white md:text-gray-800"></i>
@@ -17,12 +17,12 @@ export default function ClientNavbarComponent() {
         {/* navbar right section */}
         <div className="flex gap-x-1 md:gap-x-10 items-center">
           <div>
-            <div className="cursor-pointer hover:bg-gray-100 duration-300 box-border p-2 rounded-xl">
-              <i className="fi fi-tr-bells text-xl relative top-1 text-white md:text-black"></i>
+            <div className="cursor-pointer hover:bg-gray-100 duration-300 box-border p-2 rounded-xl text-white md:text-black hover:text-black">
+              <i className="fi fi-tr-bells text-xl relative top-1"></i>
             </div>
           </div>
           {/* account dropdown badge */}
-          <div className="flex gap-x-1 md:gap-x-4 items-center cursor-pointer hover:bg-gray-100 duration-300 box-border p-2 rounded-xl">
+          <div className="flex gap-x-1 md:gap-x-4 items-center cursor-pointer hover:bg-gray-100 duration-300 box-border p-2 rounded-xl group">
             <div>
               <img src={avartar} className="w-10 h-10 rounded-[50%]" />
             </div>
@@ -32,7 +32,7 @@ export default function ClientNavbarComponent() {
                 <div className="text-gray-400 text-xs">SS3 (WAEC)</div>
               </div>
               <div>
-                <i className="fi fi-tr-caret-down relative top-1 text-white md:text-black"></i>
+                <i className="fi fi-tr-caret-down relative top-1 text-white md:text-black group-hover:text-black"></i>
               </div>
             </div>
           </div>
