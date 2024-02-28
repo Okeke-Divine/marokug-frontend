f97b54e054e8ee03502a04bd298d0ccab3fa7a0a
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-02-28 16:09:05 +0100

    Updated

--- src/routes/client/components/ClientNavbarComponent.jsx
+++ src/routes/client/components/ClientNavbarComponent.jsx
@@ -3,7 +3,7 @@ import avartar from "../../../assets/images/avartar/male.jpg";
 export default function ClientNavbarComponent() {
   return (
     <>
-      <div className="sticky top-0 bg-white p-5 pt-3 pb-3 flex justify-between items-center">
+      <div className="sticky top-0 bg-gray-800 md:bg-white p-5 pt-3 pb-3 flex justify-between items-center">
         <div className="p-2 pr-3 pl-3 rounded-xl border-gray-200 border-2 flex gap-x-2 wd-fit">
           <input
             type="search"
@@ -11,14 +11,14 @@ export default function ClientNavbarComponent() {
             className="bg-transparent w-[8rem] md:w-40 lg:w-80 outline-none"
           />
           <button>
-            <i className="fi fi-rr-search text-gray-800"></i>
+            <i className="fi fi-rr-search text-white md:text-gray-800"></i>
           </button>
         </div>
         {/* navbar right section */}
         <div className="flex gap-x-1 md:gap-x-10 items-center">
           <div>
             <div className="cursor-pointer hover:bg-gray-100 duration-300 box-border p-2 rounded-xl">
-              <i className="fi fi-tr-bells text-xl relative top-1"></i>
+              <i className="fi fi-tr-bells text-xl relative top-1 text-white md:text-black"></i>
             </div>
           </div>
           {/* account dropdown badge */}
@@ -32,7 +32,7 @@ export default function ClientNavbarComponent() {
                 <div className="text-gray-400 text-xs">SS3 (WAEC)</div>
               </div>
               <div>
-                <i className="fi fi-tr-caret-down relative top-1"></i>
+                <i className="fi fi-tr-caret-down relative top-1 text-white md:text-black"></i>
               </div>
             </div>
           </div>
