648bffe09dd038dbf8a72493296757ee9a7f1a72
Okeke Divine-Vessel <okekedivine.skiy1@gmail.com>
2024-03-08 20:41:44 +0100

    added svgs/images

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Frontend code for (https://marokug.vercel.app)
 ## CREDITS
  - Icons: Flaticons by <a href="https://www.flaticon.com/uicons" target="_blank">Flaticon</a>
  - Fonts: Kode Mono by <a href="https://fonts.google.com/" target="_blank">Google Font</a>
+
+https://www.pixeltrue.com/premium/education-illustration-pack
--- src/routes/landingPage/main.jsx
+++ src/routes/landingPage/main.jsx
@@ -5,19 +5,21 @@ export default function LandingPage({ Config }) {
     <>
       <div className="flex justify-center items-center h-screen">
         <div>
-        <div className="font-bold text-xl">Welcome to {Config.site_name}.</div>
-      <div>
-        The landing page is still under construction.
-        <br />
-        In the mean time, vist the{" "}
-        <Link
-          to="/home"
-          className="ml-1 text-blue-400 hover:text-blue-500 duration-100 hover:border-b-2 border-dotted border-blue-500"
-        >
-          Home
-        </Link>{" "}
-        page.
-      </div>
+          <div className="font-bold text-xl">
+            Welcome to {Config.site_name}.
+          </div>
+          <div>
+            The landing page is still under construction.
+            <br />
+            In the mean time, vist the{" "}
+            <Link
+              to="/home"
+              className="ml-1 text-blue-400 hover:text-blue-500 duration-100 hover:border-b-2 border-dotted border-blue-500"
+            >
+              Home
+            </Link>{" "}
+            page.
+          </div>
         </div>
       </div>
     </>
